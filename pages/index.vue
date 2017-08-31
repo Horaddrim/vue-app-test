7c9054e986b26e69e809f8a79faf2843e9615571
Igor <lee12rock@gmail.com>
2017-08-31 15:12:20 -0300

    Added some template changes

--- pages/index.vue
+++ pages/index.vue
@@ -3,14 +3,13 @@
     <div>
       <logo/>
       <h1 class="title">
-        vue-app-test
+        Igor Franca
       </h1>
       <h2 class="subtitle">
-        Nuxt.js project
+        Back-end Developer in Brazil :D
       </h2>
       <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
+        <a href="https://github.com/Horaddrim" target="_blank" class="button--grey">GitHub</a>
       </div>
     </div>
   </section>
